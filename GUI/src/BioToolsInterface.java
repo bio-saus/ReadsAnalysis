2c0a97b879287a63b5445e93784ed0ca899412f4
Matthijs Kuik <matthijs.kuik@gmail.com>
2014-09-15 13:59:47 +0200

    Hello World

---
+++ GUI/src/BioToolsInterface.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Matthijs on 15-09-14.
+ */
+public class BioToolsInterface extends JFrame {
+
+    public BioToolsInterface(Dimension FRAMESIZE) {
+
+        setSize(FRAMESIZE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}
